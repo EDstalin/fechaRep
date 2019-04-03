e7c9a2d2542a50e5511374c911360036dab81495
Vespertino <Vespertino@C4PC13>
2019-04-03 17:03:29 +0200

    primer cambio

---
+++ examenEntonros/src/examenEntonros/FechaTest.java
@@ -0,0 +1,14 @@
+package examenEntonros;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FechaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
